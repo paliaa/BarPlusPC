c452faf72687fa8ebfa7fbcfbf3a632357de9f0d
paliaa <pat_mahlknecht@users.noreply.github.com>
2020-07-15 10:29:11 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using BarPlus.Viewmodels;
+using BarPlus.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BarPlus
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static MainWindow app;
+        public static MainWindow App
+        {
+            get { return app; }
+            set { app = value; }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Makes the MainWindow object public so that it and its public methods are visible in the whole project.
+            MainWindow.App = this;
+
+            SwitchPage(new MainViewModel());
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchPage(new MainViewModel());
+        }
+
+        //-----------------------[ PUBLIC METHODS ]-----------------------
+
+        /// <summary>The method <c>SwitchPage</c> is used to change the page of the application.
+        /// <example>For example:
+        /// <code>
+        ///    MainWindow.App.SwitchPage(new MainViewModel());
+        /// </code>
+        /// changes the current page to MainView.
+        /// </example>
+        /// </summary>
+        /// <param name="page">the ViewModel of the new page.</param>
+        public void SwitchPage(Object page)
+        {
+            DataContext = page;
+        }
+    }
+}
---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BarPlus.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BarPlus.Views
+{
+    /// <summary>
+    /// Interaktionslogik für MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_kassa_Click(object sender, RoutedEventArgs e)
+        {
+            //MainWindow.App.SwitchPage(new KassaViewModel());
+        }
+        private void Click_statistic(object sender, RoutedEventArgs e)
+        {
+            MainWindow.App.SwitchPage(new StatisticViewModel());
+        }
+    }
+}
