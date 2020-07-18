1b2596754fef75921322f6c9281d0f9275d9019f
paliaa <pat_mahlknecht@users.noreply.github.com>
2020-07-18 10:07:40 +0200

    NumberPad erweitert
    Nummern werden ins Pad eingetragen
    Del-Funktion wurde hinzugefügt

--- Views/BarMenu.xaml.cs
+++ Views/BarMenu.xaml.cs
@@ -27,49 +27,220 @@ namespace BarPlus.Views
 
         private void btn_click_1(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 1;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if(tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
 
         private void btn_click_2(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 2;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_3(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 3;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_4(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 4;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_5(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 5;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_6(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 6;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_7(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 7;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_8(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 8;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_9(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 9;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
         private void btn_click_0(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            int num = 0;
 
+            if (tb == "")
+            {
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb == "0")
+            {
+                tb_kassa.Clear();
+                tb_kassa.Text = num.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + num.ToString();
+            }
         }
 
         private void btn_del(object sender, RoutedEventArgs e)
         {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
 
+            if (tb.Length > 1)
+            {
+                tb = tb.Substring(0, tb.Length - 1);
+            }
+            else
+            {
+                tb = "0";
+            }
+            tb_kassa.Text = tb.ToString();
         }
 
         private void btn_multi(object sender, RoutedEventArgs e)
@@ -90,6 +261,22 @@ namespace BarPlus.Views
         private void btn_click_result(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void btn_click_comma(object sender, RoutedEventArgs e)
+        {
+            //Variablen
+            string tb = tb_kassa.Text.ToString();
+            String comm = ",";
+
+            if (tb == "")
+            {
+                tb_kassa.Text = comm.ToString();
+            }
+            else if (tb != "")
+            {
+                tb_kassa.Text = tb_kassa.Text + comm.ToString();
+            }
         }
     }
 }
