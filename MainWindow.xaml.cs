0b21e988494fee05e5dcb068738b5c73d8cb4207
paliaa <68185086+paliaa@users.noreply.github.com>
2020-08-29 11:56:47 +0200

    Time created

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BarPlus
 {
@@ -38,6 +39,20 @@ namespace BarPlus
             MainWindow.App = this;
 
             SwitchPage(new MainViewModel());
+        }
+
+        public void DispatcherTimerSample()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            main_time.Content = DateTime.Now.ToLongTimeString();
         }
 
         private void QuitButton_Click(object sender, RoutedEventArgs e)
