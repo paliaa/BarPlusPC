3655b9f07ce62b45997e2b45757de930cd834d4b
paliaa <pat_mahlknecht@users.noreply.github.com>
2020-08-31 08:50:30 +0200

    Create CREATE STATEMENTS.sql

---
+++ SQL/CREATE STATEMENTS.sql
@@ -0,0 +1,19 @@
+CREATE TABLE t_products (
+    p_id int,
+    p_name varchar(255),
+    p_groupid int,
+    p_price decimal,
+    p_ivaid int
+);
+
+CREATE TABLE t_iva (
+    i_id int,
+    i_name varchar,
+    i_value int
+);
+
+CREATE TABLE t_group (
+    g_id int,
+    g_name varchar
+)
+
