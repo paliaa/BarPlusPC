033974676be96e75132b2f48ef02bf47912dd767
paliaa <pat_mahlknecht@users.noreply.github.com>
2020-09-07 21:53:01 +0200

    Update SQL Statements

--- SQL/CREATE STATEMENTS.sql
+++ SQL/CREATE STATEMENTS.sql
@@ -1,9 +1,11 @@
 CREATE TABLE t_products (
+	id int NOT NULL AUTO_INCREMENT,
     p_id int,
     p_name varchar(255),
     p_groupid int,
     p_price decimal(6,2),
-    p_ivaid int
+    p_ivaid int,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE t_iva (
--- SQL/INSERT STATEMENTS.sql
+++ SQL/INSERT STATEMENTS.sql
@@ -1,7 +1,9 @@
 INSERT INTO t_group values ('1', "BAR");
 INSERT INTO t_group values ('2', "RISTORANTE");
 
-INSERT INTO t_products ('1', 'MACCHIATO', '1', '1.30', '1');
-INSERT INTO t_products ('2', 'ESPRESSO', '1', '1.30', '1');
-INSERT INTO t_products ('3', 'CAFFE LUNGO', '1', '1.30', '1');
-
+INSERT INTO t_products VALUES (null, 1, "ESPRESSO", 1, 10.00, 1);
+INSERT INTO t_products VALUES (Null, 2, "CAPPUCCINO", 1, 10.00, 1);
+INSERT INTO t_products VALUES (Null, 3, "MACCHIATO", 1, 10.00, 1);
+INSERT INTO t_products VALUES (Null, 4, "CAFFE LUNGO", 1, 10.00, 1);
+INSERT INTO t_products VALUES (Null, 5, "BRIOCHES", 1, 10.00, 1);
+INSERT INTO t_products VALUES (Null, 1, "PIZZA", 2, 10.00, 1);
